f1a47bfa621ebc12d3090c5aa1acf1ebaecdea9e
fullnelson14 <bradjnelson14@gmail.com>
2018-09-11 13:11:32 -0600

    adding redux logger

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
-import { createStore, combineReducers } from "redux";
+import { applyMiddleware, createStore, combineReducers } from "redux";
 import listReducer from "./reducers/listReducer";
 import counterReducer from "./reducers/counterReducer";
+import logger from "redux-logger";
 
 const store = createStore(
-  combineReducers({ counters: counterReducer, lists: listReducer })
+  combineReducers({ counters: counterReducer, lists: listReducer }),
+  applyMiddleware(logger)
 );
 
 export default store;
